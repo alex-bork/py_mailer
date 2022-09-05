edde346f3a885daf26349e3c22b6ff2615d1a385
Alex Bork <alex.bork@outlook.com>
2022-09-05 14:28:40 +0200

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='abork_mailer',
-    version='1.0.0', 
+    version='1.0.1', 
     packages=['py_mailer'], 
     author='Alex Bork',
     description='Mailer Library'
